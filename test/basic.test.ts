e8c0b57bf46967bd5a3e1dbd81081077bc0c271e
Julian Kern <kern.julian@gmx.de>
2022-01-14 11:02:56 +0100

    update

---
+++ test/basic.test.ts
@@ -0,0 +1,20 @@
+import { expect, test, assert } from 'vitest';
+import sqrt from '../src/index';
+
+// Edit an assertion and save to see HMR in action
+
+test('Math.sqrt()', () => {
+  expect(sqrt(4)).toBe(2);
+  expect(sqrt(144)).toBe(12);
+  expect(sqrt(2)).toBe(Math.SQRT2);
+});
+
+test('JSON', () => {
+  const input = {
+    foo: 'hello',
+    bar: 'world',
+  };
+  const output = JSON.stringify(input);
+  expect(output).eq('{"foo":"hello","bar":"world"}');
+  assert.deepEqual(JSON.parse(output), input, 'matches original');
+});
